75a21532ea832ac78f5b17cd85d75ce87a245fee
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-04-07 14:44:57 +0530

    Create 07_rulesOfJSX.js

---
+++ 07_rulesOfJSX.js
@@ -0,0 +1,7 @@
+//JSX works like HTML,but we can enter "javascript mode" by using  {} (for text or attributes)
+//We can place javascript expressions inside {}.
+// statement are not allowed (if , else, switch,for)
+//A piece of JSX can have only one root element 
+
+JSX (JavaScript XML) is an extension to the JavaScript language syntax, often used with React, a popular JavaScript library for building user interfaces. JSX resembles HTML but it's not exactly the same. Here are some key differences between JSX and HTML:
+  
