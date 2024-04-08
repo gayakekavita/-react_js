c6f725f48dee0d59430104747326e414c830f1fa
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-04-08 11:38:56 +0530

    Create 15_mechanismofState.js

---
+++ 15_mechanismofState.js
@@ -0,0 +1,8 @@
+/*
+ We  do not .do direct DOM manipulation because react is declarative not impurative
+ In a react view is updated by re-rendering the component.
+ React calls the component fuction again.
+ State is prereserved throughout the re-renders.
+ React reacts to state changes by re-rendering the user interface .
+ 
+*/
