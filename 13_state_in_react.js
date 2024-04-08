54afdf86dae8cd3dee10a65b5197612dbf24bdd6
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-04-08 11:08:15 +0530

    Create 13_state_in_react.js

---
+++ 13_state_in_react.js
@@ -0,0 +1,23 @@
+/*
+   State is most important concept in react.
+   Following are the options which are used to use state in react:
+      1.useState
+      2.useReducer
+      3.Context API 
+
+    State is a data that a component can hold over time , necessary for the information that it needs to remember throughout the app's lifecycle .
+    State is also referred as component's memory.
+    Examples are:
+      1.Notification count
+      2.Message count
+
+    State variable/ piece of state:
+       A single variable in a component 
+
+    Updating component state triggers react to re-render the component.
+
+    State allows devlopers:
+       1.Update the components view (by re-rendering the components)
+       2.persist local variables between the renders .
+       
+*/
