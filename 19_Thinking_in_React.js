b56235d2019996fd0c3c62ea0d0e4f19b067d6a5
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-04-14 19:50:25 +0530

    Create 19_Thinking_in_React.js

---
+++ 19_Thinking_in_React.js
@@ -0,0 +1,19 @@
+Following are things that should be think when you are a react devloper::
+1. How to work with react API.
+2.Thinking in react
+
+above are two things when you become master in it then you can create professional react app
+--We think about state, components and dataflow,effects etc.
+--Thinking in  state transitions.
+-------------------------------------
+  Thinking in react process::
+1. Break the desired UI into componets and establish the component here.
+2.Build a static version of react(without state).
+3.Think about state:
+    -- when to use state
+    -- Types of state (local or global)
+    --where to place each piece of state 
+4.Establish data flow:
+   -- one way data flow
+   -- Child-to-parent communication
+   --Accessing global state.
